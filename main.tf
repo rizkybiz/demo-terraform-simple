af190d15547f060c6466055036f217ec2714d7b7
rizkybiz <justin.defrank@hashicorp.com>
2021-09-28 18:38:28 -0400

    upd: created security group resource to allow access into the EC2 instance

--- main.tf
+++ main.tf
@@ -17,6 +17,45 @@ provider "aws" {
   region = var.region
 }
 
+# create a security group to allow certain types
+# of traffic In/Out to our VM
+resource "aws_security_group" "demo_security_group" {
+  name = "${var.prefix}-demo-ws-security-group"
+
+  ingress {
+    description = "TLS into VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "HTTP intp VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "SSH into VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    description      = "Allow all outbound traffic"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  tags = {
+    name = "${var.prefix}-demo-web-server-security-group"
+  }
+}
+
 # create an EC2 instance to serve as the compute
 # for our web server
 resource "aws_instance" "vm_1" {
@@ -24,7 +63,6 @@ resource "aws_instance" "vm_1" {
   instance_type               = var.instance_type
   key_name                    = aws_key_pair.demo_key_pair.key_name
   associate_public_ip_address = true
-  subnet_id                   = aws_subnet.demo_subnet.id
   vpc_security_group_ids      = [aws_security_group.demo_security_group.id]
 
   tags = {
@@ -36,16 +74,13 @@ resource "aws_instance" "vm_1" {
 # copy over the index.html local file and use shell
 # to install some deps
 resource "null_resource" "configure-web-server" {
-  depends_on = [
-    aws_eip_association.demo_vm_eip_assoc
-  ]
   triggers = {
     build_number = timestamp()
   }
 
   provisioner "file" {
-    source      = "./files"
-    destination = "/home/ubuntu"
+    source      = "./files/setup.sh"
+    destination = "/home/ubuntu/setup.sh"
     connection {
       type        = "ssh"
       user        = "ubuntu"
@@ -61,7 +96,8 @@ resource "null_resource" "configure-web-server" {
       "sudo apt -y install apache2",
       "sudo systemctl start apache2",
       "sudo chown -R ubuntu:ubuntu /var/www/html",
-      "chmod +x *.sh",
+      "sudo ls -ll -a",
+      "chmod +x setup.sh",
       "PLACEHOLDER=${var.placeholder} WIDTH=${var.width} HEIGHT=${var.height} PREFIX=${var.prefix} ./setup.sh",
     ]
     connection {
